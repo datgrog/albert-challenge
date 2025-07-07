c26c018430d8f12ac75f5c5c5553261daad9c2e6
Antoine Guidis <antoine@spot.so>
2025-07-07 17:47:59 +0200

    chore: setup Alert Home Challenge project with Flask,  Poetry, /ping route, and dev tooling
    - Added pyproject.toml with Flask, Gunicorn, Black, Ruff, Mypy, and Taskipy
    - Created basic src layout with app.py and /ping route
    - Included README.md and .gitignore

---
+++ src/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, Response, jsonify
+
+app = Flask(__name__, static_url_path="/v0")
+
+
+@app.route("/ping")
+def ping() -> Response:
+    return jsonify({"message": "pong ping"})
